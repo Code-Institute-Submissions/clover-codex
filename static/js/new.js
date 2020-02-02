d33a90cded915bc9439ebcc4194f6b14be692af4
Stephen Kasolo <kasolos@hotmail.co.uk>
2020-02-02 22:42:29 +0000

    added back to top button

--- static/js/new.js
+++ static/js/new.js
@@ -1,12 +1,4 @@
 
-var loadFile = function(event) {
-    var output = document.getElementById('output');
-    output.src = URL.createObjectURL(event.target.files[0]);
-  }; 
-
-function test(){
-    console.log("test");
-    };
 
 
 
