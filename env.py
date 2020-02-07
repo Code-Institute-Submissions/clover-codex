e0a355f6f244aede83afe32441fb43412ead1c6c
Stephen Kasolo <kasolos@hotmail.co.uk>
2020-02-07 23:14:54 +0000

    edit gitignore

--- env.py
+++
@@ -1,3 +0,0 @@
-import os 
-os.environ["MONGO_URI"] = "mongodb+srv://admin:Password1@myfirstcluster-qjzok.mongodb.net/clover_codex?retryWrites=true&w=majority"
-os.environ["SECRET_KEY"] = "PLUS_ULTRA"
